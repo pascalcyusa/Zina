b0bc2d7ab911de4f194524bf4e20901f63ec6061
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2024-12-24 15:36:51 -0600

    Initial Commit

---
+++ Zina/ZinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZinaApp.swift
+//  Zina
+//
+//  Created by Pascal Cyusa on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct ZinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZinaTests/ZinaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZinaTests.swift
+//  ZinaTests
+//
+//  Created by Pascal Cyusa on 12/24/24.
+//
+
+import Testing
+@testable import Zina
+
+struct ZinaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
